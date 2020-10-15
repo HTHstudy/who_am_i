ea3f61d702a12bba47fb74610afcab0f85211f2e
하태현 <taehyen11@gmail.com>
2020-10-15 19:42:47 +0900

    3rd commit (quiz)

---
+++ quiz.js
@@ -0,0 +1,33 @@
+init();
+
+function init() {
+  const data = [
+    {
+      question: "저는 몇살 일까요?",
+      anwser: "1993년에 태어난 28살입니다.",
+    },
+    {
+      question: "저의 혈액형은 무엇일까요?",
+      anwser: "O형 입니다.",
+    },
+    {
+      question: "저는 어디에 살까요?",
+      anwser: "강남구 대치동에 거주하고 있습니다.",
+    },
+    {
+      question: "저의 고향은 어디일까요?",
+      anwser: "부산입니다.",
+    },
+    {
+      question: "저의 MBTI 유형은 무엇일까요?",
+      anwser: "ENTJ 입니다.",
+    },
+    {
+      question: "저의 취미는 무엇일까요?",
+      anwser:
+        "딱히 취미라고 할만한건 없는거 같고 술도 좋아하고 사람도 좋아합니다.",
+    },
+  ];
+
+  console.log(data[1].anwser);
+}
