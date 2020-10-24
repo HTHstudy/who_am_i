a0f7d5ffa39fdc5b5127bc97ff6b01cdbb71f2ef
하태현 <hataehyeon@gimjun-ui-MacBookAir.local>
2020-10-24 22:01:43 +0900

    viewblock add

--- game.js
+++ game.js
@@ -1,32 +1,76 @@
-init();
-// 초급 9*9
-// 중급 16*16
-// 고급 30*16
-function init() {
+(function () {
   let randomArr = [];
+  let checkArr = [];
+  let score = 10;
   const lowRow = 9;
   const lowColumn = 9;
 
-  tagCreate();
   randomNumber();
-  viewStatus();
+  createTag();
+  openBlock();
 
-  function viewStatus() {
-    for (let i = 0; i < lowRow; i++) {
-      for (let j = 0; j < lowColumn; j++) {
-        document.getElementById(`${i}-${j}`).textContent = randomArr[i][j];
+  function openBlock() {
+    const $block = document.querySelectorAll(".block");
+    $block.forEach((block) => {
+      block.addEventListener("click", function () {
+        const id = block.id;
+        const row = id.substr(0, 1);
+        const column = id.substr(2, 1);
+
+        viewBlock(row, column);
+      });
+    });
+  }
+  function viewBlock(row, column) {
+    const aroundMine = randomArr[row][column];
+
+    const isClicked = document
+      .getElementById(`${row}-${column}`)
+      .getAttribute("isclicked");
+    if (aroundMine === "m") {
+      console.log("gameover");
+    }
+    if (aroundMine !== 0) {
+      if (isClicked === "false") {
+        document.getElementById(`${row}-${column}`).innerHTML =
+          randomArr[row][column];
+      }
+
+      document
+        .getElementById(`${row}-${column}`)
+        .setAttribute("isclicked", true);
+      return;
+    } else {
+      // 주변 지뢰 0일때
+      document
+        .getElementById(`${row}-${column}`)
+        .setAttribute("isclicked", true);
+      for (let i = row - 1; i < row + 2; i++) {
+        for (let j = column - 1; j < column + 2; j++) {
+          if (
+            i < 0 ||
+            j < 0 ||
+            i >= lowRow ||
+            j >= lowColumn ||
+            (i === row && j === column) ||
+            isClicked === "true"
+          ) {
+            continue;
+          }
+          viewBlock(i, j);
+        }
       }
     }
   }
 
-  function installMine(chkRow, chekColumn) {
+  function checkMine(chkRow, chekColumn) {
     for (let i = chkRow - 1; i < chkRow + 2; i++) {
       for (let j = chekColumn - 1; j < chekColumn + 2; j++) {
         if (
           i < 0 ||
           j < 0 ||
-          i === lowRow ||
-          j === lowColumn ||
+          i >= lowRow ||
+          j >= lowColumn ||
           (i === chkRow && j === chekColumn)
         ) {
           continue;
@@ -56,24 +100,24 @@ function init() {
         i--;
       } else {
         randomArr[randomRow][randomcolumn] = "m";
-        installMine(randomRow, randomcolumn, randomArr);
+        checkMine(randomRow, randomcolumn, randomArr);
       }
     }
     console.log(randomArr);
   }
 
-  function tagCreate() {
-    const $gameBoard = document.querySelector(".game-board");
+  function createTag() {
+    const $app = document.querySelector("#app");
 
     for (let i = 0; i < lowRow; i++) {
       for (let j = 0; j < lowColumn; j++) {
         const $block = document.createElement("div");
         $block.classList.add("block");
         $block.id = `${i}-${j}`;
-        $block.style.height = `${720 / lowColumn}px`;
-        $block.style.width = `${720 / lowRow}px`;
-        $gameBoard.appendChild($block);
+        $block.setAttribute("isclicked", false);
+        $block.setAttribute("isminecheck", false);
+        $app.appendChild($block);
       }
     }
   }
-}
+})();
